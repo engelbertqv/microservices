ceed8ec3d48c2196ea0852e94cf2b147960af611
Engelbert Quiroz <engelbertqv@gmail.com>
2018-01-18 16:00:57 -0500

    first commit

---
+++ place/src/main/java/com/place/entity/Place.java
@@ -0,0 +1,38 @@
+package com.place.entity;
+
+public class Place {
+
+	private long id;
+	private String name;
+
+	/**
+	 * @return the ID
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the NAME
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
